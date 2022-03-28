b408f641ab5dde7d609e2ae863b0315e2e4b2c8f
Tejender Upadhyay <2020kucp1019@iiitkota.ac.in>
2022-03-28 13:57:53 +0530

    heroku deploy

--- app.py
+++ app.py
@@ -146,5 +146,6 @@ def chat(text):
         return chatbot.translate(answer, "English", session["language-name"])
     return chatbot.chat(text)
 
+port = int(os.environ.get("PORT", 5000))
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', port=port)
