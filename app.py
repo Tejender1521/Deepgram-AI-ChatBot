90ac9a418b9934ea0d24b6e978eb93adcdd88462
Tejender Upadhyay <2020kucp1019@iiitkota.ac.in>
2022-03-28 14:21:29 +0530

    final check

--- app.py
+++ app.py
@@ -146,6 +146,6 @@ def chat(text):
         return chatbot.translate(answer, "English", session["language-name"])
     return chatbot.chat(text)
 
-port = int(os.environ.get("PORT", 5000))
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=port)
+    app.run(debug=True)
